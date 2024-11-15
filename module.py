f90885e2f92a1ada1ace9e289f95c40af56ba733
Xinyao Qian <xinyaoq2@illinois.edu>
2024-11-14 21:17:52 -0600

    Update module.py

--- module.py
+++ module.py
@@ -1,1 +1,17 @@
+"""
+
+Goal: Read in raw Edgar files, downloadable from the
+...
+
+module.py is an auxiliary file that defines the following classes
+(1) EdgarAnalyzer
+
+See Example.py for implementation of the EdgarAnalyzer functions
+"""
+
+class EdgarAnalyzer(object):
+    """
+    Object class to read in Edgar 10k Data
+
+    """
 
