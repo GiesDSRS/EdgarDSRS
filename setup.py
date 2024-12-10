bcb1fb61a8def3e5e0d996bed31be12a5d12b635
PratikR <52060680+pratikrelekar@users.noreply.github.com>
2024-12-10 17:02:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="edgartool",
-    version="0.1.0",
-    packages=find_packages(),
+    version="0.1.0",  # Update this with your new version
+    packages=find_packages(exclude=["tests*"]),
     install_requires=[
         'beautifulsoup4>=4.9.3',
-        'lxml>=4.9.0',
+        'lxml>=4.9.0'
     ],
     author="Pratik Relekar, Xinyao Qian",
-    author_email="pratik.relekar@gmail.com, xinyaoq2@illinois.edu",
+    author_email="relekar2@illinois.edu, xinyaoq2@illinois.edu",
     description="A tool for cleaning SEC EDGAR HTML files",
-    long_description=open('README.md').read(),
-    url="https://github.com/pratikrelekar/edgar_tool",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pratikrelekar/Edgartool",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires=">=3.6",
 )
